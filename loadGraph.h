17c9ad8a9deb588209534a10610918800581470f
Bacmen <78918918+jakubbacka03@users.noreply.github.com>
2023-05-13 09:13:00 +0200

    Update loadGraph.h

--- loadGraph.h
+++ loadGraph.h
@@ -5,19 +5,19 @@
 #include <vector>
 #include <queue>
 #include <numeric>
+#include <list>
 
 using namespace std;
 
 class loadGraph
 {
 private:
-	void AddEdge(int from, int to, vector<vector<int>>& adjacencyMatrix);
-	int DFS(int startVertex, vector<bool>& visited, vector<vector<int>>& adjacencyMatrix);
-	int BFS(int startVertex, vector<bool>& visited, vector<vector<int>>& adjacencyMatrix);
-	int findLargestComponent(vector<vector<int>>& adjacencyMatrix);
-	void FindEccentricities(vector<vector<int>>& adjacencyMatrix);
+	void AddEdge(int from, int to, vector<list<int>>& adjacencyList);
+	int DFS(int startVertex, vector<bool>& visited, vector<list<int>>& adjacencyList);
+	int BFS(int startVertex, vector<bool>& visited, vector<list<int>>& adjacencyList);
+	int findLargestComponent(vector<list<int>>& adjacencyList);
+	void FindEccentricities(vector<list<int>>& adjacencyList);
 
 public:
 	loadGraph(string path);
 };
-
